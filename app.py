dfe5d59ee7398ded82519110463d492b9fda8428
Rohith <mr.rohith.paruchuri@gmail.com>
2025-03-30 22:33:40 +0530

    V2 with inteface and logout as well as session

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect, request
+from flask import Flask, render_template, url_for, redirect, request, flash, session
 import sqlite3
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -9,16 +9,23 @@ def get_db_connection():
     return conn
  
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = True
+app.config["SESSION_TYPE"] = "filesystem" 
 app.secret_key = "einlskyehkl345j jopiw34;klj648jlkrj84kl5320kdlfmyt6'd'l[p34kuj3p]"
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    profileSec = session.get('profileSec', 0)
+    uName = session.get('username', 'None')
+    return render_template('index.html', profileSec=profileSec)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == "POST":
+        if session.get('profileSec') == 1:
+            return redirect(url_for("profile"))
+        
         username = request.form.get("username")
         password = request.form.get("password") 
         conn = get_db_connection()
@@ -28,11 +35,14 @@ def login():
         user = cursor.fetchone()
         
         conn.close()
-        
+
         if not user:
             return "No User Name Found"
         elif check_password_hash(user["password"], password):
-            return "Successful Login!"
+            session['username'] = username
+            session['profileSec'] = 1
+            flash(f"Thanks for posting!", category="success")
+            return redirect(url_for("profile"))
         else:
             return "Login Failed"
 
@@ -61,6 +71,19 @@ def register():
 
         return redirect(url_for('login')) 
     return render_template('register.html')
+
+@app.route('/profile', methods=['GET', 'POST'])
+def profile():
+    if session.get('profileSec') != 1:
+        flash("You must be logged in to access the profile page.", category="danger")
+        return redirect(url_for('login'))
+    return render_template('profile.html', username = session.get('username'))
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    session['profileSec'] = 0
+    session['username'] = 'None'
+    return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.run(debug=True)
